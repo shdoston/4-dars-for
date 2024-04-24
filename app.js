031b296861a3a5657b1efb0f09ef619be72cc2f0
Dostonbek <dostonbek200gmail.com>
2024-04-24 22:58:21 +0500

    4-oy 4-dars

---
+++ app.js
@@ -0,0 +1,65 @@
+const number = + prompt("Raqamni kiriting");
+for (let i = 1; i <=10; i++){
+    console.log(`${number} x ${i} = ${number * i}`);
+}
+
+
+// let text = 
+// "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium nam iure qui, at quaerat eum eaque, labore ea consectetur dolor beatae dolorem nulla veritatis sequi odio corrupti necessitatibus quas eligendi!";
+// let caunter = 0
+// for (let i = 0; i < text.length; i++){
+//     if (text[i].toLowerCase() == "a"){
+//         caunter++;
+//     }
+// }
+// console.log(caunter);
+
+
+
+// let text = 
+// "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium nam iure qui, at quaerat eum eaque, labore ea consectetur dolor beatae dolorem nulla veritatis sequi odio corrupti necessitatibus quas eligendi!";
+// let caunter = 0
+// for (let i = 0; i < text.length; i++){
+//     if (text[i].toLowerCase() == "r"){
+//         caunter++;
+//         break;
+//     }
+//     console.log(1);
+// }
+// if (caunter){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// const number1 = +prompt("Birinchi soni kiriting:");
+// const belgi = prompt("belgini kiriting:");
+// const number2 = prompt("ikkinchi sonni kiriting:");
+// switch(belgi){
+//     case "+":
+//         console.log(number1+number2);
+//         break;
+//         case "-":
+//             console.log(number1-number2);
+//             break;  
+// }
+
+// let son1 = +prompt(" k sonni kiriting:");
+// let son2 = +prompt("n sonni kriting");
+// for (let i = 0; i < n; i++){
+//   if(n>0){
+//   butunson(n);
+//   } else{
+//     console.log("n soni musbat")
+//   }
+// }
+// let son1 = prompt("a sonni kiriting");
+// let son2 = prompt("b sonni kiriting")
+// for (let i = 0; i <= b; i++ ){
+//     if ( a > b){
+//       console.log{i}
+//     }
+//     } 
+    
+
+
